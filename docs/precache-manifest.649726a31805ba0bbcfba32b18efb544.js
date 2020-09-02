600661fc8f40753a22b6d513fe3ef115fb1a98e4
Eduardo Calzone <Eduardo.Calzone@gmail.com>
2020-09-02 10:53:34 -0300

    docs agregados

---
+++ docs/precache-manifest.649726a31805ba0bbcfba32b18efb544.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e602e214eadd9a5dfd259ff045ee645",
+    "url": "/index.html"
+  },
+  {
+    "revision": "75a83ed00abc8194271b",
+    "url": "/static/css/main.a7046d30.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75a83ed00abc8194271b",
+    "url": "/static/js/main.bb0e1813.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
---
+++ src/components/AddCategory.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+
+export const AddCategory = ({ setCategories }) => {
+  const [inputValue, setInputValue] = useState("");
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (inputValue.length > 2) {
+      setCategories(categs => [inputValue, ...categs]);
+      setInputValue("");
+    }
+    //console.log("submit");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" value={inputValue} onChange={handleInputChange} />
+    </form>
+  );
+};
+
+AddCategory.propTypes = {
+  setCategories: PropTypes.func.isRequired,
+};
